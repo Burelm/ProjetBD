45f87ed4dbe5f6f643bf72bc02333b20bce8debb
escribis <antonin.fornara@etu.univ-grenoble-alpes.fr>
2021-03-15 17:28:06 +0100

    Update LesAdressesClients.java

--- Code/ProjetBD/src/Metier/LesAdressesClients.java
+++ Code/ProjetBD/src/Metier/LesAdressesClients.java
@@ -1,8 +1,37 @@
 package Metier;
 
 public class LesAdressesClients {
-	private int idAdresse;
-	private String noClient = "";
-	private String prenom = "";
-	private String mail = "";
+	private String nomRue;
+	private String ville;
+	private String codePostal;
+	
+	public LesAdressesClients(String nomRue, String ville, String codePostal) {
+		this.nomRue = nomRue;
+		this.ville = ville;
+		this.codePostal = codePostal;
+	}
+
+	public String getNomRue() {
+		return nomRue;
+	}
+
+	public void setNomRue(String nomRue) {
+		this.nomRue = nomRue;
+	}
+
+	public String getVille() {
+		return ville;
+	}
+
+	public void setVille(String ville) {
+		this.ville = ville;
+	}
+
+	public String getCodePostal() {
+		return codePostal;
+	}
+
+	public void setCodePostal(String codePostal) {
+		this.codePostal = codePostal;
+	}
 }
