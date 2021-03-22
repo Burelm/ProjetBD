0e2448d83b65934842250075920d4e91ed25165e
l3miage-hebertco <67104829+l3miage-hebertco@users.noreply.github.com>
2021-03-22 14:43:55 +0100

    Update ProduitDAO.java

--- Code/ProjetBD/src/DAO/ProduitDAO.java
+++ Code/ProjetBD/src/DAO/ProduitDAO.java
@@ -1,1 +1,20 @@
+package ressource;
+import java.sql.*;
 
+public class ProduitDAO extends DAO<Produit>{
+    
+    public ProduitDAO(Connection conn) { 
+        super(conn); 
+    }
+    
+    public Produit read(int id) {
+        Produit produit = new Produit();      
+        try {
+            ResultSet result = this.connect.createStatement().executeQuery("SELECT * FROM LesClients WHERE noClient ="+id);
+            if(result.next()) 
+                client = new Client(id,result.getString("addr_mail"),result.getString("nom"),result.getString("prenom"),result.getString("mdp"));
+            } catch (SQLException e) { e.printStackTrace(); }
+        return client;  
+        }
+    
+}
