21824867a8941006a148e66ee8909281ead2ecd6
escribis <antonin.fornara@etu.univ-grenoble-alpes.fr>
2021-03-22 14:32:25 +0100

    DAO

--- Code/ProjetBD/src/DAO/LesAdressesClientsDAO.java
+++ Code/ProjetBD/src/DAO/LesAdressesClientsDAO.java
@@ -1,5 +1,14 @@
 package DAO;
+import java.sql.Connection;
+import Metier.LesAdressesClients;
 
-public class LesAdressesClientsDAO {
 
+public class LesAdressesClientsDAO extends DAO<LesAdressesClients>{
+	public LesAdressesClientsDAO(Connection conn) {
+		super(conn);
+	}
+	
+	public LesAdressesClients read(int id) {
+		LesAdressesClients lesAdressesClients = new ArrayList<AdresseClient>
+	}
 }
---
+++ Code/ProjetBD/src/Metier/AdresseClient.java
@@ -0,0 +1,32 @@
+package Metier;
+
+public class AdresseClient {
+	String nomRue;
+	String numeroRue;
+	String ville;
+	String code_postal;
+	public String getNomRue() {
+		return nomRue;
+	}
+	public void setNomRue(String nomRue) {
+		this.nomRue = nomRue;
+	}
+	public String getNumeroRue() {
+		return numeroRue;
+	}
+	public void setNumeroRue(String numeroRue) {
+		this.numeroRue = numeroRue;
+	}
+	public String getVille() {
+		return ville;
+	}
+	public void setVille(String ville) {
+		this.ville = ville;
+	}
+	public String getCode_postal() {
+		return code_postal;
+	}
+	public void setCode_postal(String code_postal) {
+		this.code_postal = code_postal;
+	}
+}
