2d2ca87340448c034bb488dc1fa76eba1436e5f3
l3miage-burelm <67075801+l3miage-burelm@users.noreply.github.com>
2021-03-22 18:30:08 +0100

    Update panierDAO.java

--- Code/ProjetBD/src/DAO/panierDAO.java
+++ Code/ProjetBD/src/DAO/panierDAO.java
@@ -6,19 +6,23 @@ import java.sql.SQLException;
 
 
 import Metier.Panier;
-
+import Metier.Produit;
+import Metier.Produit.TypeI;
+import Metier.Produit.TypeQ;
 
 public class panierDAO extends DAO<Panier> {
+	
 	public panierDAO(Connection conn) { 
 		super(conn);
 	}
 	
-	public boolean create(Panier obj) {
+	
+	public boolean create(int id,Panier obj) {//à finir
 		try {
 			this.connect.createStatement().executeQuery(
 			"INSERT INTO Panier"
 			+ "VALUES("
-			+obj.getIDCommande()+","+obj.getIDProduit()+")");
+			+obj.()+")");
 			return true;
 			}
 		
@@ -30,23 +34,22 @@ public class panierDAO extends DAO<Panier> {
 		
 	}
 
-	public  Panier read(int id) {
-		Panier panier = null;
-		try {
-			ResultSet result=this.connect.createStatement().executeQuery(
-			"SELECT * FROM Panier where idCommande =="+id);
-			if (result.first()) {
-				panier = new Panier(id,result.getInt("idProduit"));
-			}
-			return panier;	
-		}
-		catch (SQLException e) {
-			e.printStackTrace();
-			}
-		return panier;
-		
-	}
-	public  boolean update(Panier obj) {
+	
+	public  Panier read(int id) { //fait
+	    Panier lePanier = new Panier();
+        try {
+            ResultSet result = this.connect.createStatement().executeQuery("SELECT * FROM Produit INNER JOIN PANIER ON"+
+            	"PRODUIT.idProduit=PANIER.idProduit"+" where PANIER.IdCommande="+id);
+            while (result.next()) {
+            	lePanier.addTolistProduit(new Produit(result.getInt("idProduit"),TypeI.valueOf(result.getString("typeImpr")),TypeQ.valueOf(result.getString("Qualité")),result.getFloat("Prix"),result.getString("Description")));
+            }
+
+        } catch (SQLException e) { e.printStackTrace(); }
+        return lePanier;
+    }
+	
+	
+	public  boolean update(Panier obj) { //à faire
 		boolean res=delete(obj);
 		if(res) {
 		res=create(obj);
@@ -55,7 +58,8 @@ public class panierDAO extends DAO<Panier> {
 		return res;
 	}
 	
-	public  boolean delete(Panier obj) {
+	
+	public  boolean delete(Panier obj) { //àfaire
 		try {
 			this.connect.createStatement().executeQuery(
 			"DELETE FROM Panier"
