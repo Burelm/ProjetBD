92ce0304d38a4721e8d37c2be45367d373e2c1dd
l3miage-kailir <77158282+l3miage-kailir@users.noreply.github.com>
2021-03-24 18:10:32 +0100

    Update imageDateUpdater.sql

--- Code/ProjetBD/sql/imageDateUpdater.sql
+++ Code/ProjetBD/sql/imageDateUpdater.sql
@@ -1,5 +1,5 @@
 CREATE OR REPLACE TRIGGER imageDateUpdater
-AFTER INSERT ON Tirage
+AFTER INSERT,UPDATE ON Tirage
 FOR EACH ROW
 BEGIN
   UPDATE FichierImage SET DateUtilisation = SYSDATE() WHERE idImage = :new.idImage;
