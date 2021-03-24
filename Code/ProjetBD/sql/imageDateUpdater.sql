ade5987a3e014403749ebe85e70c25f97a3a07eb
l3miage-burelm <burelmarvin.pro@gmail.com>
2021-03-24 19:43:10 +0100

    Espère que ça casse rien

--- Code/ProjetBD/sql/imageDateUpdater.sql
+++
@@ -1,7 +0,0 @@
-CREATE OR REPLACE TRIGGER imageDateUpdater
-AFTER INSERT OR UPDATE ON Tirage
-FOR EACH ROW
-BEGIN
-  UPDATE FichierImage SET DateUtilisation = SYSDATE() WHERE idImage = :new.idImage;
-END;
-/
--- Code/ProjetBD/sql/promoDeleter.sql
+++
@@ -1,7 +0,0 @@
-CREATE OR REPLACE TRIGGER promoDeleter
-AFTER INSERT ON LesCommandes
-FOR EACH ROW
-BEGIN
-  DELETE CodePromo WHERE noClient = :new.noClient AND :new.Promo = 1 AND ROWNUM <= 1;
-END;
-/
