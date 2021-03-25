7985c12c162dcc752fff3e04e30c1503eb06e95a
l3miage-kailir <77158282+l3miage-kailir@users.noreply.github.com>
2021-03-25 10:33:53 +0100

    Create promoCreator.sql

---
+++ Code/ProjetBD/sql/promoCreator.sql
@@ -0,0 +1,13 @@
+CREATE OR REPLACE TRIGGER promoCreator
+AFTER INSERT ON LesCommandes
+FOR EACH ROW
+  DECLARE 
+   sumPrice INT;
+   countNbPromo INT;
+BEGIN
+  SELECT COUNT(*) INTO countNbPromo FROM CodePromo;
+  SELECT SUM(PRIX) INTO sumPrice FROM (SELECT PRIX, C.idCommande FROM LesCommandes C JOIN Panier P ON C.idCommande = P.idCommande JOIN Produit A ON P.idProduit = A.idProduit WHERE noClient = :new.noClient);
+  IF sumPrice > 100 THEN INSERT INTO CodePromo VALUES (countNbPromo+1,:new.noClient);
+  END IF;
+END;
+/
